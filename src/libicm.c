e0c207867c10a72b776bfd79c1ee18569e301fcc
Sylvain Saucier <sylvain@sysau.com>
2023-05-28 08:56:55 -0600

    0.1.0
    Tuned the algorithm to pass practrand.

--- src/libicm.c
+++ src/libicm.c
@@ -1,7 +1,7 @@
 /**
 * @file libicm.c
 * @author Sylvain Saucier <sylvain@sysau.com>
-* @version 0.4.0
+* @version 0.1.0-BETA
 * @section LICENSE *
 * This program is free software; you can redistribute it and/or
 * modify it under the terms of the Affero GNU Public Licence version 3.
@@ -11,6 +11,7 @@
 
 #include <pthread.h>
 #include <stdint.h>
+#include <unistd.h>
 
 #include "libicm.h"
 
@@ -24,9 +25,8 @@
 */
 int ___libicm_modify( volatile uint64_t* in, volatile uint64_t* out )
 {
-    if(in == NULL || out == NULL)
-        return 1;
-        
+/* read before modifying https://sysau.com/Derivatives.pdf */
+    if(in == NULL || out == NULL) return 1;
     uint64_t acc = 1;                                   /* newvalue : 128 unique prime numbers will be multiplied together in this accumulator */
     uint64_t primes[128] =                              /* primes : list of prime numbers, 256 first prime numbers. */
     #include "primes.large.txt"
@@ -35,9 +35,9 @@ int ___libicm_modify( volatile uint64_t* in, volatile uint64_t* out )
         *in = (*in << 13) | (*in >> (64 - 13));         /* source is rotated by 13 bits... */
         acc = (acc << x) | (acc >> (64 - x));           /* smooth out bit distribution in acc */
         acc *= primes[(2 * x)+(1 & *in)];               /* accumulate a unique prime for this run... */
-        *out ^= acc;                                    /* add uncertainty in the sink too as we go... */
+        *out += acc ^ *in;                                    /* add uncertainty in the sink too as we go... */
     }
-    //*out ^= acc;                                      /* finally XOR the accumulated product to the sink */
+    *out ^= acc;                                      /* finally XOR the accumulated product to the sink */
     return 0;
 }
 
@@ -48,7 +48,7 @@ int ___libicm_modify( volatile uint64_t* in, volatile uint64_t* out )
 * This function pause all the ICM threads
 */
 void __icm_pause(icm_state_t* icm){
-    for(int x = 0; x < _ICM_MAX_THREADS; x++){
+    for(int x = 0; x < _ICM_NUM_THREADS; x++){
         pthread_mutex_lock(&(icm->threads[x].mutx));
         icm->threads[x].pause = 1;}}
 
@@ -60,7 +60,7 @@ void __icm_pause(icm_state_t* icm){
 * This function pause all the ICM threads
 */
 void __icm_start(icm_state_t* icm){
-    for(int x = 0; x < _ICM_MAX_THREADS; x++){
+    for(int x = 0; x < _ICM_NUM_THREADS; x++){
         pthread_mutex_unlock(&(icm->threads[x].mutx));}}
 
 /**
@@ -77,11 +77,14 @@ void* ___libicm_threadwork(void* raw)
         ___libicm_modify(state->source, state->sink);
         if(state->pause)
         {
-            if(!state->run) pthread_exit(NULL);
+            if(!state->run)
+            {
+                pthread_exit(NULL);
+            }
             state->pause = 0;
             pthread_mutex_lock(&state->mutx);               //waiting for restart
             pthread_mutex_unlock(&state->mutx);             //unlocking the mutex right away...
-            }
+        }
     }
     return NULL;
 }
@@ -94,18 +97,21 @@ void* ___libicm_threadwork(void* raw)
 */
 void icm_init(icm_state_t* state)
 {
-    for( int i = 0; i < _ICM_MAX_THREADS; i++ ){
+    for( int i = 0; i < _ICM_NUM_THREADS; i++ )
+    {
         state->nodes[i] = 0;
         state->threads[i].source = &(state->nodes[i]);
-        state->threads[i].sink = &(state->nodes[(i + 1) % _ICM_MAX_THREADS]);
+        state->threads[i].sink = &(state->nodes[(i + 1) % _ICM_NUM_THREADS]);
         state->threads[i].run = 1;
         state->threads[i].pause = 1;
         pthread_mutex_init(&(state->threads[i].mutx), NULL);
         pthread_mutex_lock(&(state->threads[i].mutx));
-        pthread_create(&(state->threads[i].thr), NULL, &___libicm_threadwork, &(state->threads[i]));}
+        pthread_create(&(state->threads[i].thr), NULL, &___libicm_threadwork, &(state->threads[i]));
+    }
     __icm_start(state);
     usleep(_ICM_WARMUP);
-    __icm_pause(state);}
+    __icm_pause(state);
+}
 
 /**
 * @brief ICM Stop function, stops all threads
@@ -115,9 +121,15 @@ void icm_init(icm_state_t* state)
 */
 void icm_stop(icm_state_t* state)
 {
-    for(int x = 0; x < _ICM_MAX_THREADS; x++) state->threads[x].run = 0;
+    for(int x = 0; x < _ICM_NUM_THREADS; x++)
+    {
+        state->threads[x].run = 0;
+    }
     __icm_pause(state);
-    for(int x = 0; x < _ICM_MAX_THREADS; x++) pthread_join(state->threads[x].thr, NULL);
+    for(int x = 0; x < _ICM_NUM_THREADS; x++)
+    {
+        pthread_join(state->threads[x].thr, NULL);
+    }
 }
 
 /**
@@ -128,15 +140,21 @@ void icm_stop(icm_state_t* state)
 * @param count uint64_t how many uint64_t to wriye
 * This function fills n bytes in a buffer
 */
-void icm_fill64(icm_state_t* state, uint64_t* buffer, uint64_t count){
+void icm_fill64(icm_state_t* state, uint64_t* buffer, uint64_t count)
+{
     __icm_start(state);
     uint64_t answer = 0;
-    for( int i = 0; i < count; i++ ){
+    for(int i = 0; i < count; i++)
+    {
         usleep(_ICM_WAIT);
-        for( int y = 0; y < _ICM_MAX_THREADS; y++ )
+        for(int y = 0; y < _ICM_NUM_THREADS; y++ )
+        {
             answer ^= state->nodes[y];
-        buffer[i] = answer;}
-    __icm_pause(state);}
+        }
+        buffer[i] = answer;
+    }
+    __icm_pause(state);
+}
 
 /**
 * @brief ICM XOR Mix Function
@@ -144,12 +162,18 @@ void icm_fill64(icm_state_t* state, uint64_t* buffer, uint64_t count){
 * @param raw void* pointer to an icm_thread_t
 * Like icm_fill but with ^= instead of =.
 */
-void icm_mix64(icm_state_t* state, uint64_t* destination, uint64_t count){
+void icm_mix64(icm_state_t* state, uint64_t* destination, uint64_t count)
+{
     __icm_start(state);
     uint64_t answer = 0;
-    for( int i = 0; i < count; i++ ){
+    for( int i = 0; i < count; i++ )
+    {
         usleep(_ICM_WAIT);
-        for( int y = 0; y < _ICM_MAX_THREADS; y++ )
+        for( int y = 0; y < _ICM_NUM_THREADS; y++ )
+        {
             answer = state->nodes[y];
-        destination[i] ^= answer;}
-    __icm_pause(state);}
+        }
+        destination[i] ^= answer;
+    }
+    __icm_pause(state);
+}
--- src/libicm.h
+++ src/libicm.h
@@ -1,7 +1,7 @@
 /**
 * @file libicm.h
 * @author Sylvain Saucier <sylvain@sysau.com>
-* @version 0.4.0
+* @version 0.1.0-BETA
 * @section LICENSE *
 * This program is free software; you can redistribute it and/or
 * modify it under the terms of the Affero GNU Public Licence version 3.
@@ -14,27 +14,28 @@
 
 #include <pthread.h>
 #include <stdint.h>
-#include <stdio.h>
-#include <unistd.h>
-#include <time.h>
 
-#define _ICM_WARMUP         1000                    // Warmup after init in microseconds
-#define _ICM_WAIT           30                      // Sleeping time in microseconds
-#define _ICM_MAX_THREADS    3                       // number of threads and nodes in ICM (3 recommended)
+#define _ICM_WARMUP         1000  // Warmup after init in microseconds
+#define _ICM_WAIT           30    // Sleeping time in microseconds between picks
+#define _ICM_NUM_THREADS    3     // number of threads and nodes in ICM (3 recommended)
 
-typedef struct{
+typedef struct
+{
     volatile uint64_t *source;
     volatile uint64_t *sink;
     int pause;
     int run;
     pthread_mutex_t mutx;
     pthread_t thr;
-} icm_thread_t;
+}
+icm_thread_t;
 
-typedef struct{
-    volatile uint64_t nodes[_ICM_MAX_THREADS];
-    icm_thread_t threads[_ICM_MAX_THREADS];
-} icm_state_t;
+typedef struct
+{
+    volatile uint64_t nodes[_ICM_NUM_THREADS];
+    icm_thread_t threads[_ICM_NUM_THREADS];
+}
+icm_state_t;
 
 void icm_init(icm_state_t* state);
 void icm_fill64(icm_state_t* state, uint64_t* buffer, uint64_t count);
